2c143509bfba88f0b1c1889c4b2ccaf084ca5870
MostofaAsik <mmostofaasik@gmail.com>
2023-05-03 06:55:36 +0600

    createing json file

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefdata = require("./data/chefData.json")
+
 app.get('/', (req, res) => {
     res.send("Chef Are Coming")
+})
+
+app.get('/chefdata', (req, res) => {
+    res.send(chefdata)
+
 })
 
 app.listen(port, () => {
