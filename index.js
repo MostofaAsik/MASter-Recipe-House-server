6fa9173519e85a26a3653d7eb72299d0127040d7
MostofaAsik <mmostofaasik@gmail.com>
2023-05-03 07:42:52 +0600

    updated cors

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 const chefdata = require("./data/chefData.json")
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send("Chef Are Coming")
